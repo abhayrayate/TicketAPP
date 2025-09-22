558418fb6886fda1cc2742d3bf97e6da2e112843
Abhay Rayate <you@example.com>
2025-09-22 22:50:07 +0530

    eleven

--- src/main/java/in/abhayit/Application.java
+++ src/main/java/in/abhayit/Application.java
@@ -3,7 +3,15 @@ package in.abhayit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	    servers = {
+	        @Server(url = "https://ticketapp.up.railway.app", description = "Railway Prod Server")
+	    }
+	)
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/in/abhayit/RestController/TicketRestController.java
+++ src/main/java/in/abhayit/RestController/TicketRestController.java
@@ -5,7 +5,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
